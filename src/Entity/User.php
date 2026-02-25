b0afafb17eb06f57d94ac5c95b13dca852f453df
VillainAlexandre <villainalexandre89@gmail.com>
2026-02-25 18:06:03 +0100

    rajout de la connection + de la création de compte

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,24 +4,33 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['password'], message: 'There is already an account with this password')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 50, unique: true)]
     private ?string $pseudo = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $motDePass = null;
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->pseudo;
     }
 
     public function getPseudo(): ?string
@@ -29,22 +38,30 @@ class User
         return $this->pseudo;
     }
 
-    public function setPseudo(string $pseudo): static
+    public function setPseudo(string $pseudo): self
     {
         $this->pseudo = $pseudo;
-
         return $this;
     }
 
-    public function getMotDePass(): ?string
+    public function getPassword(): string
     {
-        return $this->motDePass;
+        return $this->password;
     }
 
-    public function setMotDePass(string $motDePass): static
+    public function setPassword(string $password): self
     {
-        $this->motDePass = $motDePass;
-
+        $this->password = $password;
         return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+        public function eraseCredentials(): void
+    {
+        // Vide les données sensibles temporaires si besoin
     }
 }
